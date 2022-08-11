e55c659f2d02893b10a16da1783f29463db2d59e
amaurus <aimforthemia@gmail.com>
2022-08-11 15:31:50 +0300

    5

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 console.log('hi')
 var randomWords = ["Взрыв", "Пещера", "Принцесса", "Карандаш"]; 
-var randomIndex = Math.floor(Math.random() * 4); 
+var randomIndex = Math.floor(Math.random() * 5); 
 console.log(randomWords[randomIndex]);
+
